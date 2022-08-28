957118da282e54d561872d27a7de25aff2097959
CahildiBrat <99288895+CahildiBrat@users.noreply.github.com>
2022-08-28 19:16:58 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    BOT_TOKEN = "12345:sadasdadasdasdasdasdadasd"# Bot token
+    API_ID = 12345678 # my.telegram.orgdan alın
+    API_HASH = "asdasdasdasdsadasdasdadasdasdas" # my.telegram.orgdan alın
+    TEXT = """☺️ Salam dostum necəsən?\n\n🔔 Mən Quliyev`in botuyam. \n❤️ Aşağıda Güncəlləmə kanalı və Musiqi playlist kanalına baxa bilərsən ❤️"""
+    KANAL_ADI_1 = "ᴍᴜʟᴛɪ ᴀᴢ ᴏғғɪᴄɪᴀʟ" #Buttonun adı
+    KANAL_LINKI_1 = "t.me/MultiAzOfficial" # Butonun linki 
+    KANAL_ADI_2 = "ᴍᴜʟᴛɪᴀᴢ ᴘʟᴀʏʟɪsᴛ" #Butonun adi
+    KANAL_LINKI_2 = "https://t.me/MultiPlaylist" # Butonun linki
